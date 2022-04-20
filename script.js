6ea9a8f3735fafda3dca25022f2cd3d81b448fc2
Fabricio Gomes <54582091+fahfgomes@users.noreply.github.com>
2022-04-19 23:40:31 -0300

    Inclusão de informações
    Neste commit ocorreu a inclusão do sabores da pizzzas, os seus valores e suas imagens

--- script.js
+++ script.js
@@ -6,30 +6,32 @@
         Autor do script.js - Fabricio Freitas - github: fahfgomes
         Autor dos demais arquivos: B7WEB
    */ 
-   
-   
-   
-   
-   
    /*
         Armazenando em uma constante os dados que serão recebidos pela arrow function,
         para que o código fique mais limpo, assim chamamos a função c, recebemos os dados necessários
         e retornamos o o documento html relacionado ao dado que foi fornecido
     */
-    const c = (el)=>{
-        return document.querySelector(el);
-    }
+   const s = (el)=>document.querySelector(el);
+   const sall = (el)=>document.querySelectorAll(el);
     
     pizzaJson.map((item, index)=>{
 
         //clonando todo o conteúdo que possuimos de modelo, para prencher posteriormente
 
-        let pizzaItem = document.c(".models .pizza-item").cloneNode(true);
+        let pizzaItem = s('.models .pizza-item').cloneNode(true);
         /*
         preencher as informações em pizza item
         utilizar o "append" pois ele adiciona o item
         o innerHTML neste caso iria substituir os itens, e não seria o que a gente gostaria no momento
         pois teremos mais de uma pizza no catálogo*/
 
-        c(".pizza-area").append(pizzaItem);
+        pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+        pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+        pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+        pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+        
+        
+
+
+        s('.pizza-area').append(pizzaItem);
     })
